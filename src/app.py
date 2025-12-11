b9aaf596570ec60e1d9baf91df39b7972286a923
Dylan Seabaugh <djsfby@umsystem.edu>
2025-12-11 21:27:04 +0000

    made app and test

--- src/app.py
+++ src/app.py
@@ -1,2 +1,41 @@
+import math
+
 def add (a, b):
     return a+b
+
+def sub (a, b):
+    return a-b
+
+def mult (a, b):
+    return a*b
+
+def div (a, b):
+    if b == 0:
+        return "no negative"
+    return a/b
+
+def log (a):
+    if a == 0:
+        return "no negative"
+    return math.log(a)
+
+def sqr (a):
+    return a ** 2
+
+def cos (a):
+    return math.cos(a)
+
+def sin (a):
+    return math.sin(a)
+
+def sqr_root (a):
+    if a >= 0:
+        return math.sqrt(a)
+    else:
+        return "no negatives"
+
+def per (a, b):
+    if a >= 0 & b >= 0:
+        return (a/b) * 100
+    else:
+        return "no negatives"
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,35 @@
 import sys
+import math
 from pathlib import Path
 
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
-from app import add
+from app import add, sub, mult, div, log, sqr, cos, sin, sqr_root, per
 
 def test_add():
     assert add(5, 6) == 11
 
-def test_add2():
-    assert add(5, 6) != 10
+def test_sub():
+    assert sub(10, 6) == 4
 
+def test_mult():
+    assert mult(4, 5) == 20
+
+def test_div():
+    assert div(12, 3) == 4
+
+def test_log():
+    assert log(100) == 2
+
+def test_square():
+    assert sqr(4) == 16
+
+def test_sin():
+    assert sin(0) == 0
+
+def test_cos():
+    assert cos(math.pi) == -1
+
+def test_percent():
+    assert per (6, 10) == 60
